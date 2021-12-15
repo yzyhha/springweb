bba81e10610868822ab5f4ed349ef67fc39d78af
yzyhha <2502209046@qq.com>
2021-12-15 10:34:25 +0800

    change

--- demo/src/main/java/com/example/demo/controller/GreetingController.java
+++ demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -4,15 +4,17 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.demo.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+
 public class GreetingController {
     private static final String template = "Hello,%s!";
     private final AtomicLong counter = new AtomicLong();
-
+    @PostMapping("/greeting")
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
